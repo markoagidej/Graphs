1a8f9f23972df461ca971e1ee5d196b673816e63
Marko <markogidej@yahoo.com>
2024-08-10 16:30:19 -0400

    Init Commit

---
+++ Solving Shortest Path Problem with Dijkstra's Algorithm.py
@@ -0,0 +1,44 @@
+# Task 1: Define Graph Representation
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.vertices:
+            self.vertices[vertex] = {}
+
+    def add_edge(self, source, destination, weight):
+        if source in self.vertices and destination in self.vertices:
+            self.vertices[source][destination] = weight
+            self.vertices[destination][source] = weight  # For undirected graph
+
+    def get_neighbors(self, vertex):
+        if vertex in self.vertices:
+            return self.vertices[vertex]
+        else:
+            return {}
+
+# Example usage:
+graph = Graph()
+graph.add_vertex('A')
+graph.add_vertex('B')
+graph.add_vertex('C')
+graph.add_edge('A', 'B', 5)
+graph.add_edge('B', 'C', 3)
+graph.add_edge('A', 'C', 10)
+
+# Task 2: Implement Dijkstra's Algorithm
+
+
+
+# Task 3: Test the Algorithm Implementation
+
+
+
+# Task 4: Analyze Time and Space Complexity
+
+'''
+Time Completixy is 
+Space Complexity is
+'''
